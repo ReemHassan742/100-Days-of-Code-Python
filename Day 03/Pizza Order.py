2bc72c77b8e666115568925d10f829ca1b708cb0
Reem Hassan <reemhassan742r@gmail.com>
2024-12-09 07:04:51 +0200

    Add files via upload

---
+++ Day 03/Pizza Order.py
@@ -0,0 +1,31 @@
+print("Welcome to Python Pizza Deliveries")
+size = input("What size Pizza do you want? S, M or L: ")
+pepperoni = input("DO you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra chese? Y or N: ")
+
+# todo: work out how much they need to pay based on their size choice. 
+
+bill = 0 
+if size == "S": 
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25 
+else:
+    print("You typed the wrong inputs.")
+
+# todo: work out how much to add to their bill based on their pepperoni choice.
+
+if pepperoni == "Y":
+    if size == "S":
+       bill += 2
+    else:
+        bill += 3
+        
+# todo: work out their final amount based on whether if they want extra cheese.
+
+if extra_cheese == "Y":
+    bill += 1 
+    
+print(f"Your final bill is: ${bill}.")
